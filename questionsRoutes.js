535f47159c1e5fbf4242176d7605a70fde6a15a8
Aeroloutre <166110416+Aeroloutre@users.noreply.github.com>
2025-07-23 09:20:12 +0000

    id autoincrement + POST (not working properly yet)

--- questionsRoutes.js
+++ questionsRoutes.js
@@ -8,41 +8,64 @@ const router = express.Router()
 
 // Route GET /questions
 router.get('/questions', async (req, res) => {
+  try {
   const questions = await prisma.questions.findMany()
-  res.json(questions)
+  res.json(questions)}
+  catch (error) {
+    console.error(error.message)
+    res.sendStatus(500).json({ error: error.message })
+  }
 })
 
 // Route PUT /questions pour mettre à jour une question par index
 router.put('/questions/:id/firstchoicecount', async (req, res) => {
   const questionIndex = parseInt(req.params.id)
-  console.log("index de la question à put",questionIndex)
+  console.log("index de la question à put", questionIndex)
+  try {
   const updatedQuestion = await prisma.questions.update({
-      where: { id: questionIndex },
-      data: {firstchoicecount: {increment: 1}}
+    where: { id: questionIndex },
+    data: { firstchoicecount: { increment: 1 } }
   })
 
-  res.json(updatedQuestion)
+  res.json(updatedQuestion)}
+  catch (error) {
+    console.error(error.message)
+    res.sendStatus(500).json({ error: error.message })
+  }
 })
 
 router.put('/questions/:id/secondchoicecount', async (req, res) => {
   const questionIndex = parseInt(req.params.id)
-  console.log("index de la question à put",questionIndex)
+  console.log("index de la question à put", questionIndex)
+  try {
   const updatedQuestion = await prisma.questions.update({
-      where: { id: questionIndex },
-      data: {secondchoicecount: {increment: 1}}
+    where: { id: questionIndex },
+    data: { secondchoicecount: { increment: 1 } }
   })
 
-  res.json(updatedQuestion)
+  res.json(updatedQuestion)}
+  catch (error) {
+    console.error(error.message)
+    res.sendStatus(500).json({ error: error.message })
+  }
 })
 
-router.post('/questions', (req, res) => {
+router.post('/questions',async (req, res) => {
   const question = req.body.question
-
-  db.data.questions.push(question)
-
-  db.write()
-
-  res.json(question)
+  try {
+    const newQuestion = await prisma.questions.create({
+      data: {
+        firstchoice: question.firstchoice,
+        firstchoicecount: question.firstchoicecount,
+        secondchoice: question.secondchoice,
+        secondchoicecount: question.secondchoicecount
+      }
+    })
+    res.json(question)
+  } catch (error) {
+    console.error(error.message)
+    res.sendStatus(500).json({ error: error.message })
+  }
 })
 
 export { router }
